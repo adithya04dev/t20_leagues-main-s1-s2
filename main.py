17037f69b41dcf9c69ba1093f3aa0428e5861a17
adithya04dev <adithyabalagoni11@gmail.com>
2023-08-22 00:13:43 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+import streamlit as st 
+
+
+st.title('T20 Leagues Stats Page')
+
+st.write('Set-1 Leagues: PSL,SA20,ILT20,IPL,The Hundred,CPL,BBL,MLC,MSL,LPL,Afghanistan premier League,BPL')
+
+st.markdown("[Set-1 leagues Stats](https://t20leagues-main.streamlit.app/)")
+
+st.write('Set-2 Leagues: SMAT,CSA T20,T20 BLAST')
+st.markdown("[Set-2 leagues Stats](https://t20leagues-main-s2.streamlit.app/)")
+
+
