90a8eefce1d02b7d8465543f5f15dd4012e340e0
adithya04dev <127680046+adithya04dev@users.noreply.github.com>
2023-08-22 00:22:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ st.title('T20 Leagues Stats Page')
 st.write('Set-1 Leagues: PSL,SA20,ILT20,IPL,The Hundred,CPL')
 st.write('BBL,MLC,MSL,LPL,Afghanistan premier League,BPL')
 st.markdown("[Set-1 leagues Stats](https://t20leagues-main.streamlit.app/)")
+st.write('')
 
 st.write('Set-2 Leagues: SMAT,CSA T20,T20 BLAST')
 st.markdown("[Set-2 leagues Stats](https://t20leagues-main-s2.streamlit.app/)")
