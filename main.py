ee3e4c6badcea2f672321db488440831e7afc08b
adithya04dev <127680046+adithya04dev@users.noreply.github.com>
2023-08-22 00:21:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import streamlit as st
 
 st.title('T20 Leagues Stats Page')
 
-st.write('Set-1 Leagues: PSL,SA20,ILT20,IPL,The Hundred,CPL,BBL,MLC,MSL,LPL,Afghanistan premier League,BPL')
-
+st.write('Set-1 Leagues: PSL,SA20,ILT20,IPL,The Hundred,CPL')
+st.write('BBL,MLC,MSL,LPL,Afghanistan premier League,BPL')
 st.markdown("[Set-1 leagues Stats](https://t20leagues-main.streamlit.app/)")
 
 st.write('Set-2 Leagues: SMAT,CSA T20,T20 BLAST')
